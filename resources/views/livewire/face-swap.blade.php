e2ac9e7c53e44930901260c2444082ed26a82eeb
Adarsh2772 <adarshsolankurkar8367@gmail.com>
2024-06-30 15:50:53 +0530

    button view

--- resources/views/livewire/face-swap.blade.php
+++ resources/views/livewire/face-swap.blade.php
@@ -46,13 +46,26 @@
 
         <div class="text-center mb-6" style="    text-align: -webkit-center;">
             <p class="text-red">{{$error}}</p>
-            <button type="button" {{ $disabled }} wire:click="splitAndJoinImage()" class="bg-gray-700 text-white px-4 py-2 rounded-md flex items-center" wire:loading.attr="disabled">
-               <svg style="display: {{$disabled ? 'block': 'none'; }}" class="animate-spin h-5 w-5 text-white-500 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <circle class="opacity-45" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                    <path class="opacity-95" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zm9.707-5.291A7.962 7.962 0 0112 20v4c4.627 0 8.627-3.686 8.938-8.291l-3-2.647A7.962 7.962 0 0116 12z"></path>
-                </svg> 
-                Generate Image
-            </button>
+            <button 
+            type="button" 
+            {{$disabled}} 
+            wire:click="splitAndJoinImage()" 
+            class="bg-blue-600 text-white px-4 py-2 rounded-md flex items-center" 
+            wire:loading.attr="disabled">
+            
+            <svg 
+                style="display: {{$disabled ? 'block': 'none'; }}"
+                class="animate-spin h-5 w-5 text-white-500 mr-2" 
+                xmlns="http://www.w3.org/2000/svg" 
+                fill="none" 
+                viewBox="0 0 24 24">
+                <circle class="opacity-45" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path class="opacity-95" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zm9.707-5.291A7.962 7.962 0 0112 20v4c4.627 0 8.627-3.686 8.938-8.291l-3-2.647A7.962 7.962 0 0116 12z"></path>
+            </svg> 
+            
+            <span>Generate Image</span>
+        </button>
+
             
         </div>
 
